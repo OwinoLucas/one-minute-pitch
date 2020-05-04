d0c5cf1b8867f436ec660eafa96a21df76d03755
Lucas Owino <lucowish35@gmail.com>
2020-05-04 18:56:06 +0300

    restructure app folder and add navbar in home page for navigation

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from . import main
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
-# Initializing application
-app = Flask(__name__)
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
-from app import views
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ start.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY='@\xd7eA\xb5\xbc,\x8f\xdb\x85\xd9\x07'
+
+
+python3.6 manage.py server
