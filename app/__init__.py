82a380fea161ef290e266ab8a1c9158fb1052fe0
Lucas Owino <lucowish35@gmail.com>
2020-05-05 16:47:21 +0300

    added upload images function

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -10,6 +13,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
@@ -23,7 +27,9 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-
+    
+    # configure UploadSet
+    configure_uploads(app,photos)
     
     # Registering the blueprint
     from .main import main as main_blueprint
--- app/models.py
+++ app/models.py
@@ -12,6 +12,8 @@ class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     email = db.Column(db.String(255),unique = True)
     pass_secure = db.Column(db.String(255))
 
