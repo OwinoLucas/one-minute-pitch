bc1e4cadbcd323b099b4ac756b2c68f76894e1ce
Lucas Owino <lucowish35@gmail.com>
2020-05-07 16:22:11 +0300

    imported vote models to manage.py and py script

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Pitch,Comment
+from app.models import User,Pitch,Comment,Upvote,Downvote
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -11,7 +11,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Pitch = Pitch, Comment = Comment )
+    return dict(app = app,db = db,User = User,Pitch = Pitch, Comment = Comment,Upvote = Upvote,Downvote = Downvote )
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
